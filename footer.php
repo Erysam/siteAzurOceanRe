c384ac19ff90cbb0f806975267d7a5cea4220ec7
Erys <1maryseal@gmail.com>
2023-11-14 17:19:12 +0100

    picto add

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="footer">
+    <footer>
+        <a class="lien" href="mentionLeg.php">Mentions légales</a>
+        <a class="lien" href="contact.php">Contact</a>
+        <a class="lien" href="https://voilesetvoiliers.ouest-france.fr/">Presse</a>
+    </footer>
+</div>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/favicon.ico" sizes="16x16" href="images/favicon.ico" />
+    <link rel="stylesheet" type="text/css" title="style1" href="CSSpageAccueil.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <title>Bienvenue chez AzurOcéan</title>
+</head>
+
+<body class="formProp">
+
+    <div><img src="logo.jpg" alt="logo" class="logo" /></div>
+
+    <div class="en_tete"> <a class="lien" href="index.php">Accueil</a>
+        <a class="lien" href="enregChoix.php">Enregistrement</a>
+        <a class="lien" href="connexion.html">Connexion </a>
+
+    </div>
