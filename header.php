d882b3792f27875266c65ede502269f256406a85
Erys <1maryseal@gmail.com>
2023-11-30 11:25:49 +0100

    modification du nom fichierCSS

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/favicon.ico" sizes="16x16" href="image/favicon.ico" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" title="style1" href="cssAzurOcean.css" />
+    <link rel="stylesheet" type="text/css" title="style1" href="azurOceanCss.css" />
     <title>Bienvenue chez AzurOcéan</title>
 </head>
 
