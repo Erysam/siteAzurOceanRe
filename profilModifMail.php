e4ff6cf82b1f699977fe84e9b8ae493d2bf0f0cf
Erys <1maryseal@gmail.com>
2024-02-01 18:33:10 +0100

    creation des pages modif mail et mdp

---
+++ profilModifMail.php
@@ -0,0 +1,111 @@
+<?php
+
+$sessionLifetime = 1800; //  durée de la session 30mn 60sec x 30mn = 1800 sec
+session_set_cookie_params($sessionLifetime);
+
+session_start();
+session_regenerate_id(true);
+require('header.php');
+require('fonctionsCommunes.php');
+
+if (!issetNotEmpty($_SESSION)) {
+    header('Location: connexion.php');
+}
+
+if (isset($_GET['modif']) && $_GET['modif'] === 'modifReussie') {
+    echo ('Veuillez saisir un email valide.');
+}
+
+if (isset($_GET['erreur']) && $_GET['erreur'] === 'erreurMdp') {
+    echo ('Veuillez saisir un password valide.');
+}
+
+if (isset($_GET['erreur']) && $_GET['erreur'] === 'erreurEmail') {
+    echo ('Veuillez saisir un email valide.');
+}
+
+//Si le user n'est pas deconnecté par le $sessionLifeTime ou autre
+if (!issetNotEmpty($_SESSION['user']['id'])) {
+    header('Location: connexion.php');
+}
+
+$idUserSession = $_SESSION['user']['id'];
+//$mIdMembre;
+$mEmail;
+
+
+$maCon = connexion();
+$stmt = mysqli_stmt_init($maCon);
+$sqlSelect = "SELECT email FROM membre WHERE idMembre = $idUserSession";
+
+if (mysqli_stmt_prepare($stmt, $sqlSelect)) {
+    $result = mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);
+    if (mysqli_stmt_num_rows($stmt) > 0) {
+        mysqli_stmt_bind_result($stmt, $email);
+        if (mysqli_stmt_fetch($stmt)) {
+            $mEmail = $email;
+        }
+    } else {
+        echo "Aucun résultat trouvé pour ce membre";
+    }
+}
+mysqli_stmt_close($stmt);
+mysqli_close($maCon);
+
+?>
+
+
+<div class="h4">
+    <h4>Modifier votre Email:</h4>
+</div>
+<div class="img-content">
+    <div class="img-conx">
+        <img src="image/voilierDuCiel.jpg" alt="bateaux">
+    </div>
+</div>
+<div class="h4">
+    <h6>Cliquez sur la case pour effectuer les modifications</h6>
+</div>
+
+<?php
+if (isset($_GET['erreur']) && $_GET['erreur'] === 'duplication') {
+    echo "Les données saisies existent déjà. Veuillez corriger votre saisie.";
+}
+
+?>
+
+<form action="enregModifProfil.php" method="POST" class="formConx">
+
+    <div class="formConxDiv">
+        <label for="mail">Email</label>
+        <input type="email" class="form-control" name="email" id="mail" value="<?php echo $mEmail; ?>" required>
+    </div>
+
+
+    <div class="formConxDiv">
+        <label for="mdp">Saisissez votre password pour valider les modifications</label>
+        <input type="password" class="form-control" name="mdpActuel" id="mdp" placeholder="" required>
+        <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onclick="afficheMdp()">
+            <label class="form-check-label" for="flexSwitchCheckDefault">Afficher le mot de passe</label>
+        </div>
+    </div>
+    <div class="h4">
+        <h5>
+            <font style="color:white">Afin d'activez vos modifications,
+                <br> un email vous sera envoyé pour validation
+            </font>
+        </h5>
+    </div>
+
+    <br />
+    <div class="formConxDiv">
+        <label class="buttonSub" for="enreg"> <input type="submit" id="enreg" value="Enregistrer"></label>
+    </div>
+</form>
+
+
+<?php
+include('footer.php');
+?>
---
+++ profilModifMdp.php
@@ -0,0 +1,92 @@
+<?php
+
+$sessionLifetime = 1800; //  durée de la session 30mn 60sec x 30mn = 1800 sec
+session_set_cookie_params($sessionLifetime);
+
+session_start();
+session_regenerate_id(true);
+require('header.php');
+require('fonctionsCommunes.php');
+
+if (!issetNotEmpty($_SESSION)) {
+    header('Location: connexion.php');
+}
+
+if (isset($_GET['modif']) && $_GET['modif'] === 'modifReussie') {
+    echo ('Veuillez saisir un email valide.');
+}
+
+if (isset($_GET['erreur']) && $_GET['erreur'] === 'erreurMdp') {
+    echo ('Veuillez saisir un password valide.');
+}
+
+if (isset($_GET['erreur']) && $_GET['erreur'] === 'erreurEmail') {
+    echo ('Veuillez saisir un email valide.');
+}
+
+//Si le user n'est pas deconnecté par le $sessionLifeTime ou autre
+if (!issetNotEmpty($_SESSION['user']['id'])) {
+    header('Location: connexion.php');
+}
+
+?>
+
+
+<div class="h4">
+    <h4>Modifier votre mot de passe:</h4>
+</div>
+<div class="img-content">
+    <div class="img-conx">
+        <img src="image/voilierDuCiel.jpg" alt="bateaux">
+    </div>
+</div>
+<div class="h4">
+    <h6>Pour modifier votre mot de passe veuillez saisir les informations demandées</h6>
+</div>
+
+
+<form action="enregModifMdp.php" method="POST" class="formConx" onsubmit="return verifierMotDePasse() && verifMdpChar();">
+
+    <div class="formConxDiv">
+        <label for="mail">Email</label>
+        <input type="email" class="form-control" name="email" id="mail" required>
+    </div>
+
+    <div class="formConxDiv">
+        <label for="mdp">Saisissez l'ancien password pour valider les modifications</label>
+        <input type="password" class="form-control" name="mdpActuel" id="mdp" placeholder="" required>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 mb-3">
+            <label for="pass">Nouveau password*:</label>
+            <input type="password" class="form-control" id="pass" name="mdp" minlength="8" placeholder="">
+            <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onclick="afficheMdp()">
+                <label class="form-check-label" for="flexSwitchCheckDefault">Afficher le mot de passe</label>
+            </div>
+        </div>
+
+        <div class="col-md-6 mb-3">
+            <label for="confirmPass">Confirmer le nouveau password:</label>
+            <input type="password" class="form-control" id="confirmPass" name="confirmMdp" minlength="8" placeholder="">
+
+        </div>
+    </div>
+    <div class="passwordHelp">
+        <small id="passwordHelp" class="form-text text-muted">
+            *8 caractères minimum, inclure au moins une majuscule, une minuscule, un chiffre
+            <br>
+            et un caractère spécial.
+        </small>
+    </div>
+
+    <br />
+    <div class="formConxDiv">
+        <label class="buttonSub" for="enreg"> <input type="submit" id="enreg" value="Enregistrer"></label>
+    </div>
+</form>
+
+<?php
+include('footer.php');
+?>
