22b9cdce472e371d083a0113431a1ea2caeff7ba
Erys <1maryseal@gmail.com>
2024-04-29 16:19:51 +0200

    correction d erreurs, avec la variable idSej non envoyée depuis le form de reservation et modif du if avec la second requete pour modif du status de reservation dans table sejour  + enum = s car consideré comme une string et non un int

--- enregResaSejour.php
+++ enregResaSejour.php
@@ -3,17 +3,19 @@ require_once('configSession.php');
 include('header.php');
 include('fonctionsCommunes.php');
 
+$idSejResa;
 $idUserSession;
 if (!issetNotEmpty($_SESSION)) {
     header('Location: connexion.php?resa=emptyID');
     exit;
-} else {
-    $idUserSession = $_SESSION['user']['id'];
 }
 
-$messageSejourPropMail = '
-Le séjour, que vous avez proposé, a été réservé. Vous pouvez consulter la liste sur :
-http://localhost/siteAzurOceanRe/listSejourProp.php?
+$idUserSession = $_SESSION['user']['id'];
+$mail = $_SESSION['user']['email'];
+
+
+$messageSejourPropMail = 'Vous avez reservé un séjour en bateau sur notre site. Vous pouvez consulter la liste sur :
+http://localhost/siteAzurOceanRe//listReservations.php
  
 ---------------
 Ceci est un mail automatique, Merci de ne pas y répondre.';
@@ -21,32 +23,44 @@ Ceci est un mail automatique, Merci de ne pas y répondre.';
 
 if ($_POST['idSej']) {
     $idSejResa = $_POST['idSej'];
+    $passager = $_POST['nbPassagers'];
+    $permis = $_POST['permisBat'];
     $maCon = connexion();
     $stmt = mysqli_stmt_init($maCon);
-    $sqlInsert = "INSERT INTO arzurocean.reservation(idReservation, idSej, idMembre) VALUE (NULL, ?, ?)";
+    $sqlInsert = "INSERT INTO reservation(idReservation, idSej, idMembre, passagers, permisBat) VALUES (NULL, ?, ?, ?, ?)";
     if (mysqli_stmt_prepare($stmt, $sqlInsert)) {
-        mysqli_stmt_bind_param($stmt, "iii", $idSejResa, $idUserSession);
-    }
-    try {
-        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_param($stmt, "iiii", $idSejResa, $idUserSession, $passager, $permis);
 
-        $mailInfo = mail($mail, "Le séjour que vous avez proposé a été réservé", "From emailazurocean888@gmail.com\r\nReply-To:emailazurocean888@gmail.com ", $messageSejourPropMail);
-        mysqli_stmt_close($stmt);
+        try {
+            mysqli_stmt_execute($stmt);
 
-        $stmt = mysqli_stmt_init($maCon);
-        $sqlUpdate = "UPDATE azurocean.sejour SET reservation = ? WHERE idMembre = ?";
-        if (mysqli_stmt_prepare($stmt, $sqlUpdate)) {
-            mysqli_stmt_bind_param($stmt, "si", 1, $idUserSession);
-        }
-        $result = mysqli_stmt_execute($stmt);
-        $stmt->close();
-        mysqli_close($maCon);
-        header('Location: index.php?sejourResa=reserve');
-    } catch (mysqli_sql_exception $e) {
-        if (mysqli_errno($maCon) == 1062) {
-            mysqli_close($maCon);
-            // Redir vers le form avec get erreur pour expliquer au user l'erreur (script php sur le form)
-            header('Location: séjour.php?sejourResa=duplication');
+            $mailInfo = mail($mail, "Séjour réservé", "From emailazurocean888@gmail.com\r\nReply-To:emailazurocean888@gmail.com ", $messageSejourPropMail);
+            mysqli_stmt_close($stmt);
+
+            $stmt = mysqli_stmt_init($maCon);
+            var_dump($idSejResa);
+            $reservationStatus = 1;
+            $sqlUpdate = "UPDATE sejour SET reservation = ? WHERE idSejour = ?";
+            if (mysqli_stmt_prepare($stmt, $sqlUpdate)) {
+                mysqli_stmt_bind_param($stmt, "si", $reservationStatus, $idSejResa);
+                $result = mysqli_stmt_execute($stmt);
+                $stmt->close();
+                mysqli_close($maCon);
+                header('Location: index.php?sejourResa=reserve');
+                exit;
+            }
+        } catch (mysqli_sql_exception $e) {
+            var_dump($e);
+            if (mysqli_errno($maCon) == 1062) {
+                mysqli_close($maCon);
+                // Redir vers le form avec get erreur pour expliquer au user l'erreur (script php sur le form)
+                header('Location: sejours.php?sejourResa=duplication');
+                exit;
+            }
         }
     }
+} else {
+    var_dump('>>>>>>>' . $idSejResa);
+    // header('Location: sejours.php?resa=emptyResa');
+    exit;
 }
